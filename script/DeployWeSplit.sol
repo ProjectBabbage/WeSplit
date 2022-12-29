c81b10585aa65e5ab6c73631747bd46e97173759
Quentin Garchery <garchery.quentin@gmail.com>
2022-12-29 12:45:24 +0100

    feat: update deploy script

--- script/DeployWeSplit.sol
+++ script/DeployWeSplit.sol
@@ -2,11 +2,16 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/OwnableProxy.sol";
 import "../src/WeSplit.sol";
 
 contract DeployWeSplit is Script {
+    bytes public constant emptyData = "";
+
     function run() public {
-        vm.broadcast();
-        new WeSplit();
+        vm.startBroadcast();
+        WeSplit weSplitImplementation = new WeSplit();
+        new OwnableProxy(address(weSplitImplementation), emptyData);
+        vm.stopBroadcast();
     }
 }
