428d0f7f0125623ade939aa6ec55c362a5b8fcba
Quentin Garchery <garchery.quentin@gmail.com>
2023-01-07 18:46:16 +0100

    docs: add natspec comments

--- src/Arith.sol
+++ src/Arith.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+/// @title WeSplit arithmetic library.
+/// @author Project Babbage.
 library Arith {
+    /// @notice Compute the division of `x` by `y` rounding up.
     function divUp(uint256 x, uint256 y) internal pure returns (uint256) {
         return (x + y - 1) / y;
     }
