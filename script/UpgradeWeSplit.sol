8b8fe5e03334993a18b310b710bc5cb763db5ec6
Quentin Garchery <garchery.quentin@gmail.com>
2023-01-08 00:23:47 +0100

    fix: upgrade script

--- script/UpgradeWeSplit.sol
+++ script/UpgradeWeSplit.sol
@@ -5,11 +5,13 @@ import "forge-std/Script.sol";
 import "src/WeSplit.sol";
 
 contract UpgradeWeSplit is Script {
-    function run(address _weSplitProxy) public {
+    address constant internal weSplitProxy = 0x52decE2Fd883628eA46eBae183cd9D78a81Ef916;
+
+    function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         WeSplit newWeSplitImplementation = new WeSplit();
-        WeSplit(_weSplitProxy).upgradeTo(address(newWeSplitImplementation));
+        WeSplit(weSplitProxy).upgradeTo(address(newWeSplitImplementation));
         vm.stopBroadcast();
     }
 }
