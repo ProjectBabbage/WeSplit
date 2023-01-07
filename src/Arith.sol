596c0102e3d2eff0486a75138280ed1b0cc15119
Quentin Garchery <garchery.quentin@gmail.com>
2023-01-07 18:34:38 +0100

    feat: use Arith as an internal library

--- src/Arith.sol
+++ src/Arith.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 library Arith {
-    function divUp(uint256 x, uint256 y) external pure returns (uint256) {
+    function divUp(uint256 x, uint256 y) internal pure returns (uint256) {
         return (x + y - 1) / y;
     }
 }
